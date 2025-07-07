90952ecf970ebf50c401afbb353370ddbbd9dd4c
Dan Wismer <daniel.s.wismer@gmail.com>
2025-07-07 11:06:36 -0600

    install required packages if not in system library

--- R/00_setup.R
+++ R/00_setup.R
@@ -1,6 +1,32 @@
 
 setup <- function() {
+  
+  required_pkgs <- c(
+    "assertthat",
+    "data.table",
+    "dplyr",
+    "glue",
+    "prioritizr",
+    "RcppTOML",
+    "readxl",
+    "R6",
+    "sf",
+    "stringr",
+    "terra",
+    "tibble",
+    "uuid",
+    "yaml"
+  )
+  
+  # Install missing packages
+  for (pkg in required_pkgs) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      message(sprintf("Installing %s...", pkg))
+      install.packages(pkg, repos = "https://cloud.r-project.org")
+    }
+  }  
 
+  # Read-in toml and return configs
   toml <- "setup.toml"
   configs <- RcppTOML::parseTOML(toml)
   
