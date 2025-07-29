8958b8e9ea1c3bb9a2a1559fa70aa1e2c7534d8f
Dan Wismer <daniel.s.wismer@gmail.com>
2025-07-29 14:17:58 -0600

    support batch and points

--- Python/extract_vector_to_polygon.py
+++ Python/extract_vector_to_polygon.py
@@ -1,37 +1,41 @@
 import arcpy
 import sys
 import os
+import pandas as pd
 script_folder = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(script_folder)
 import fct_vector_pull as vp
 import importlib
 importlib.reload(vp)
 
-# Get user params
-input_poly = arcpy.GetParameterAsText(0)
-input_multi_vector_params = arcpy.GetParameterAsText(1)
-arcpy.AddMessage(input_multi_vector_params)
-arcpy.AddMessage(type(input_multi_vector_params))
-input_batch = arcpy.GetParameterAsText(2)
-
 # Set environments
 arcpy.env.overwriteOutput = True
 
-# Shape and transfrom input data
+# Get user params
+input_poly = arcpy.GetParameterAsText(0)
+input_multi_vector_params = arcpy.GetParameterAsText(1)
+input_batch = arcpy.GetParameterAsText(2)
+
 # Set emtpy lists to populate
 vector_lst = []
 name_lst = []
-metric_lst = []
+unit_lst = []
 
 # Split vector features
 multi_vector_params = input_multi_vector_params.split(";")
 
 # Extract vector parameters
 for x in multi_vector_params:
-    vector, name, metric = x.split(" ")
+    vector, name, unit = x.split(" ")
     vector_lst.append(vector)
     name_lst.append(name)
-    metric_lst.append(metric)
+    unit_lst.append(unit)
+    
+if input_batch:
+  batch_df = pd.read_csv(input_batch)
+  vector_lst.extend(batch_df['full_path'].tolist())
+  name_lst.extend(batch_df['shp_name'].tolist())
+  unit_lst.extend(batch_df['unit'].tolist())
 
 # Create wtw id
 arcpy.AddField_management(input_poly, "WTWID", "LONG")
@@ -41,17 +45,21 @@ with arcpy.da.UpdateCursor(input_poly, ["WTWID"]) as cursor:
       cursor.updateRow(row)
 
 # Process each list item
-for vector, name, metric in zip(vector_lst, name_lst, metric_lst):
+l = len(vector_lst)
+counter = 1
+for vector, name, unit in zip(vector_lst, name_lst, unit_lst):
   file_name = arcpy.Describe(vector).name
-  arcpy.AddMessage(f"... Processing: {file_name}")
+  arcpy.AddMessage(f"... Processing {counter} of {l}: {file_name}")
 
   # Extract vector to polygon
   vp.vector_pull(
     vector = vector,
     polygon = input_poly,
     col_name = name,
-    metric = metric
+    unit = unit
   )
+  
+  counter += 1
   
 
 
