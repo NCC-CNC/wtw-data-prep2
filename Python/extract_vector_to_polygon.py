929badebe361dc5a0d1394ae32e26c0964f8e0c1
Dan Wismer <daniel.s.wismer@gmail.com>
2025-07-30 16:19:49 -0600

    updates

--- Python/extract_vector_to_polygon.py
+++ Python/extract_vector_to_polygon.py
@@ -13,28 +13,29 @@ arcpy.env.overwriteOutput = True
 
 # Get user params
 input_poly = arcpy.GetParameterAsText(0)
-input_multi_vector_params = arcpy.GetParameterAsText(1)
+input_multi_params = arcpy.GetParameterAsText(1)
 input_batch = arcpy.GetParameterAsText(2)
 
 # Set emtpy lists to populate
 vector_lst = []
-name_lst = []
+short_name_lst = []
 unit_lst = []
 
-# Split vector features
-multi_vector_params = input_multi_vector_params.split(";")
 
-# Extract vector parameters
-for x in multi_vector_params:
-    vector, name, unit = x.split(" ")
-    vector_lst.append(vector)
-    name_lst.append(name)
-    unit_lst.append(unit)
-    
+# Shape tool input vector parameters
+if input_multi_params:
+  multi_params = input_multi_params.split(";")
+  for x in multi_params:
+      vector, short_name, unit = x.split(" ")
+      vector_lst.append(vector)
+      short_name_lst.append(short_name)
+      unit_lst.append(unit)
+
+# Shape batch input paramters    
 if input_batch:
   batch_df = pd.read_csv(input_batch)
-  vector_lst.extend(batch_df['full_path'].tolist())
-  name_lst.extend(batch_df['shp_name'].tolist())
+  vector_lst.extend(batch_df['conversion_ready_input'].tolist())
+  short_name_lst.extend(batch_df['short_name'].tolist())
   unit_lst.extend(batch_df['unit'].tolist())
 
 # Create wtw id
@@ -47,20 +48,17 @@ with arcpy.da.UpdateCursor(input_poly, ["WTWID"]) as cursor:
 # Process each list item
 l = len(vector_lst)
 counter = 1
-for vector, name, unit in zip(vector_lst, name_lst, unit_lst):
+for vector, short_name, unit in zip(vector_lst, short_name_lst, unit_lst):
   file_name = arcpy.Describe(vector).name
   arcpy.AddMessage(f"... Processing {counter} of {l}: {file_name}")
 
-  # Extract vector to polygon
+  ## extract vector to polygon
   vp.vector_pull(
     vector = vector,
     polygon = input_poly,
-    col_name = name,
+    col_name = short_name,
     unit = unit
   )
-  
+  ## advance counter
   counter += 1
   
-
-
-
