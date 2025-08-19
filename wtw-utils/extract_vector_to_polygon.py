1d7c373cb84adcd668f7f584fa6403911cb2f9e4
Dan Wismer <daniel.s.wismer@gmail.com>
2025-08-19 11:18:27 -0600

    removed multi table params
    explicit with either using single tool inputs or dataprep.csv

--- wtw-utils/extract_vector_to_polygon.py
+++ wtw-utils/extract_vector_to_polygon.py
@@ -13,30 +13,31 @@ arcpy.env.overwriteOutput = True
 
 # Get user params
 input_poly = arcpy.GetParameterAsText(0)
-input_multi_params = arcpy.GetParameterAsText(1)
-input_batch = arcpy.GetParameterAsText(2)
+input_vect = arcpy.GetParameterAsText(1)
+input_unit = arcpy.GetParameterAsText(2)
+input_colname = arcpy.GetParameterAsText(3)
+input_csv = arcpy.GetParameterAsText(4)
 
 # Set emtpy lists to populate
 vector_lst = []
 short_name_lst = []
 unit_lst = []
 
-
-# Shape tool input vector parameters
-if input_multi_params:
-  multi_params = input_multi_params.split(";")
-  for x in multi_params:
-      vector, short_name, unit = x.split(" ")
-      vector_lst.append(vector)
-      short_name_lst.append(short_name)
-      unit_lst.append(unit)
-
-# Shape batch input paramters    
-if input_batch:
-  batch_df = pd.read_csv(input_batch)
+# Shape csv input paramters    
+if input_csv:
+  csv_file_name = os.path.basename(input_csv)
+  arcpy.AddMessage(f"{csv_file_name} provided, using batch inputs.")
+  batch_df = pd.read_csv(input_csv)
+  batch_df = batch_df[batch_df["datatype"] == "vector"] # needed to filter
   vector_lst.extend(batch_df['conversion_ready_input'].tolist())
-  short_name_lst.extend(batch_df['short_name'].tolist())
   unit_lst.extend(batch_df['unit'].tolist())
+  short_name_lst.extend(batch_df['short_name'].tolist())
+else:
+  arcpy.AddMessage("No dataprep.csv provided, using single vector input.")
+  # If no csv provided, use single vector input
+  vector_lst.append(input_vect)
+  short_name_lst.append(input_colname)
+  unit_lst.append(input_unit)
 
 # Create wtw id
 arcpy.AddField_management(input_poly, "WTWID", "LONG")
