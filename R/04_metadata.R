7ef0e1e8d0615716daf8f77466760efab6692dea
Dan Wismer <daniel.s.wismer@gmail.com>
2026-06-01 10:25:17 -0600

    formatting

--- R/04_metadata.R
+++ R/04_metadata.R
@@ -3,121 +3,181 @@
 #' Reads national WTW species and preparation metadata, combines it with
 #' raster statistics from the project’s TIF files, and generates a complete
 #' WTW metadata CSV for use in the WTW formatting script.
-#' 
+#'
 #' @param project_dir Character. Path to the project directory containing the
 #'   `tifs` folder and the output `wtw/metadata` folder.
-#'   
+#'
 #' @param natdata_dir Character. Path to the national data directory containing
-#'   WTW metadata files: 
+#'   WTW metadata files:
 #'   - `WTW_NAT_SPECIES_METADATA.xlsx`
 #'   - `WTW_NAT_PREP_METADATA.csv`
-#'   
+#'
 #' @return Side effect: writes wtw-metadata.csv file.
-#' 
+#'
 #' -----------------------------------------------------------------------------
 
 # Source function
 source("R/fct_init_metadata.R")
 create_wtw_metadata <- function(project_dir, natdata_dir) {
-  
   TIF_DIR <- file.path(project_dir, "tifs")
   WTW_SPECIES <- file.path(natdata_dir, "WTW_NAT_SPECIES_METADATA.xlsx")
   WTW_PREP <- file.path(natdata_dir, "WTW_NAT_PREP_METADATA.csv")
-  
+
   # Read-in metadata
   ## wtw prep metadata
   wtw_prep_meta <- read.csv(WTW_PREP)
-  
+
   ## wtw species metadata
   sheet_names <- readxl::excel_sheets(WTW_SPECIES)
   wtw_species_meta <- setNames(
     lapply(sheet_names, function(x) readxl::read_excel(WTW_SPECIES, sheet = x)),
     sheet_names
   )
-  
+
   # TIF list
-  tif_list <- list.files(TIF_DIR, pattern = "\\.tif$", full.names = TRUE, recursive = TRUE)
-  
+  tif_list <- list.files(
+    TIF_DIR,
+    pattern = "\\.tif$",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+
   # Init output
   df <- init_metadata()
-  
+
   ## Loop over each tiff file:
   for (i in seq_along(tif_list)) {
-    
     # Get file names
     tif_path <- tif_list[i]
     tif_base <- tools::file_path_sans_ext(basename(tif_path))
-    tif_name <- paste0(tif_base, ".tif")  
-    
+    tif_name <- paste0(tif_base, ".tif")
+
     # Get metadata associated with the file path and file name
     if (grepl("species/eccc_ch", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$ECCC_CH, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/eccc_ch")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$ECCC_CH,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/eccc_ch"
+      )
     } else if (grepl("species/eccc_sar", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$ECCC_SAR, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/eccc_sar")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$ECCC_SAR,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/eccc_sar"
+      )
     } else if (grepl("species/iucn_amph", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$IUCN_AMPH, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/iucn_amph")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$IUCN_AMPH,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/iucn_amph"
+      )
     } else if (grepl("species/iucn_bird", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$IUCN_BIRD, File == tif_name)
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$IUCN_BIRD,
+        File == tif_name
+      )
       season <- stringr::str_extract(tif_base, "(?<=_)[0-9]+(?=_)")
-      wtw_prep_meta_row <- dplyr::filter(wtw_prep_meta, short_name == paste0("IUCN_BIRD_S", season))
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        short_name == paste0("IUCN_BIRD_S", season)
+      )
     } else if (grepl("species/iucn_mamm", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$IUCN_MAMM, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/iucn_mamm")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$IUCN_MAMM,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/iucn_mamm"
+      )
     } else if (grepl("species/iucn_rept", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$IUCN_REPT, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/iucn_rept")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$IUCN_REPT,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/iucn_rept"
+      )
     } else if (grepl("species/nsc_end", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$NSC_END, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/nsc_end")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$NSC_END,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/nsc_end"
+      )
     } else if (grepl("species/nsc_sar", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$NSC_SAR, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/nsc_sar")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$NSC_SAR,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/nsc_sar"
+      )
     } else if (grepl("species/nsc_spp", tif_path)) {
-      wtw_species_meta_row <- dplyr::filter(wtw_species_meta$NSC_SPP, File == tif_name)
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, path == "themes/species/nsc_spp")
+      wtw_species_meta_row <- dplyr::filter(
+        wtw_species_meta$NSC_SPP,
+        File == tif_name
+      )
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        path == "themes/species/nsc_spp"
+      )
     } else {
       wtw_species_meta_row <- NULL
-      wtw_prep_meta_row    <- dplyr::filter(wtw_prep_meta, wtw_file_name == tif_name)
+      wtw_prep_meta_row <- dplyr::filter(
+        wtw_prep_meta,
+        wtw_file_name == tif_name
+      )
     }
-    
+
     # Read-in raster
     r <- terra::rast(tif_list[i])
-    
+
     # Get raster stats
     if (!is.factor(r)) {
       ## df
-      r_df <- terra::as.data.frame(r, na.rm=TRUE)
+      r_df <- terra::as.data.frame(r, na.rm = TRUE)
       ## number of unique value
       u_values <- nrow(unique(r_df)) |> as.numeric()
       ## max raster value
       max_value <- max(r_df) |> as.numeric() # <- CAN NOT GET MAX ON CATEGORICAL DATA
     }
-    
+
     ## FILE ----------------------------------------------------------------------
     FILE <- tif_name
-    
+
     ## TYPE ----------------------------------------------------------------------
-    TYPE <-  dplyr::pull(wtw_prep_meta_row, type)
-    
+    TYPE <- dplyr::pull(wtw_prep_meta_row, type)
+
     ## NAME ----------------------------------------------------------------------
     if (is.null(wtw_species_meta_row)) {
       NAME <- dplyr::pull(wtw_prep_meta_row, legend_name)
     } else {
       NAME <- dplyr::pull(wtw_species_meta_row, Common_Name)
     }
-    
+
     ## THEME -------------------------------------------------------------------
     THEME <- dplyr::pull(wtw_prep_meta_row, theme_group_name)
-    
+
     ## LEGEND ------------------------------------------------------------------
     LEGEND <- if (u_values > 2) "continuous" else "manual"
-  
+
     ## VALUES ------------------------------------------------------------------
     if (identical(u_values, 2) && identical(max_value, 1)) {
-      VALUES <- "0, 1" # IUCN, NSC, KBA, Includes 
+      VALUES <- "0, 1" # IUCN, NSC, KBA, Includes
     } else if (identical(u_values, 2)) {
       VALUES <- paste0("0,", max_value) # ECCC: rare case if only 2 unique values
     } else if (identical(u_values, 1)) {
@@ -125,17 +185,17 @@ create_wtw_metadata <- function(project_dir, natdata_dir) {
     } else {
       VALUES <- "" # continuous data does not need values
     }
-    
+
     ## COLOR -------------------------------------------------------------------
     short_name <- dplyr::pull(wtw_prep_meta_row, short_name)
     legend_color <- dplyr::pull(wtw_prep_meta_row, legend_color)
     COLOR <- dplyr::case_when(
-      short_name == "ECCC_CH"   & u_values == 2 ~ "#00000000, #756bb1",
-      short_name == "ECCC_CH"   & u_values == 1 ~ "#756bb1",
-      short_name == "ECCC_CH"   & LEGEND == "continuous" ~ legend_color,
-      short_name == "ECCC_SAR"  & u_values == 2 ~ "#00000000, #fb9a99",
-      short_name == "ECCC_SAR"  & u_values == 1 ~ "#fb9a99",
-      short_name == "ECCC_SAR"  & LEGEND == "continuous" ~ legend_color,
+      short_name == "ECCC_CH" & u_values == 2 ~ "#00000000, #756bb1",
+      short_name == "ECCC_CH" & u_values == 1 ~ "#756bb1",
+      short_name == "ECCC_CH" & LEGEND == "continuous" ~ legend_color,
+      short_name == "ECCC_SAR" & u_values == 2 ~ "#00000000, #fb9a99",
+      short_name == "ECCC_SAR" & u_values == 1 ~ "#fb9a99",
+      short_name == "ECCC_SAR" & LEGEND == "continuous" ~ legend_color,
       short_name == "IUCN_AMPH" & u_values == 2 ~ legend_color,
       short_name == "IUCN_AMPH" & u_values == 1 ~ "#a6cee3",
       short_name == "IUCN_BIRD" & u_values == 2 ~ legend_color,
@@ -156,14 +216,14 @@ create_wtw_metadata <- function(project_dir, natdata_dir) {
       short_name == "CPCAD" & u_values == 1 ~ "#7fbc41",
       TRUE ~ legend_color
     )
-    
+
     ## LABELS ------------------------------------------------------------------
     legend_label <- dplyr::pull(wtw_prep_meta_row, legend_label)
     LABELS <- dplyr::case_when(
-      short_name == "ECCC_CH"   & u_values == 2 ~ legend_label,
-      short_name == "ECCC_CH"   & u_values == 1 ~ "Habitat",
-      short_name == "ECCC_SAR"  & u_values == 2 ~ legend_label,
-      short_name == "ECCC_SAR"  & u_values == 1 ~ "Range",
+      short_name == "ECCC_CH" & u_values == 2 ~ legend_label,
+      short_name == "ECCC_CH" & u_values == 1 ~ "Habitat",
+      short_name == "ECCC_SAR" & u_values == 2 ~ legend_label,
+      short_name == "ECCC_SAR" & u_values == 1 ~ "Range",
       short_name == "IUCN_AMPH" & u_values == 2 ~ legend_label,
       short_name == "IUCN_AMPH" & u_values == 1 ~ "Habitat",
       short_name == "IUCN_BIRD" & u_values == 2 ~ legend_label,
@@ -183,23 +243,23 @@ create_wtw_metadata <- function(project_dir, natdata_dir) {
       short_name == "CPCAD" & u_values == 2 ~ legend_label,
       short_name == "CPCAD" & u_values == 1 ~ "included",
       TRUE ~ legend_label
-    )    
-    
+    )
+
     ## UNITS -------------------------------------------------------------------
     UNIT <- dplyr::pull(wtw_prep_meta_row, unit)
-    
+
     ## PROVENANCE --------------------------------------------------------------
     PROVENANCE <- dplyr::pull(wtw_prep_meta_row, provenance)
-    
+
     ## VISIBLE -----------------------------------------------------------------
-    VISIBLE <- dplyr::pull(wtw_prep_meta_row, visible) 
-    
+    VISIBLE <- dplyr::pull(wtw_prep_meta_row, visible)
+
     ## HIDDEN ------------------------------------------------------------------
     HIDDEN <- dplyr::pull(wtw_prep_meta_row, hidden)
-    
+
     ## DOWNLOADABLE ------------------------------------------------------------
-    DOWNLOADABLE <- dplyr::pull(wtw_prep_meta_row, downloadable)     
-    
+    DOWNLOADABLE <- dplyr::pull(wtw_prep_meta_row, downloadable)
+
     ## GOAL --------------------------------------------------------------------
     category <- dplyr::pull(wtw_prep_meta_row, category)
     GOAL <- switch(
@@ -208,29 +268,29 @@ create_wtw_metadata <- function(project_dir, natdata_dir) {
       "habitat" = "0.2",
       ""
     )
-    
+
     ## Build new national row ----
     new_row <- c(
-      TYPE, 
-      THEME, 
-      FILE, 
-      NAME, 
-      LEGEND, 
-      VALUES, 
-      COLOR, 
-      LABELS, 
-      UNIT, 
+      TYPE,
+      THEME,
+      FILE,
+      NAME,
+      LEGEND,
+      VALUES,
+      COLOR,
+      LABELS,
+      UNIT,
       PROVENANCE,
-      VISIBLE, 
-      HIDDEN, 
-      DOWNLOADABLE, 
+      VISIBLE,
+      HIDDEN,
+      DOWNLOADABLE,
       GOAL
     )
-      
+
     ## Append to DF
     df <- structure(rbind(df, new_row), .Names = names(df))
-  } 
-  
+  }
+
   # Write to csv ----
   write.csv(
     df,
--- R/fct_create_project_folder.R
+++ R/fct_create_project_folder.R
@@ -1,28 +1,100 @@
-
 create_project_folder <- function(project_dir) {
   # Create folder structure
   suppressWarnings(dir.create(project_dir))
   suppressWarnings(dir.create(file.path(project_dir, "aoi")))
   suppressWarnings((dir.create(file.path(project_dir, "tifs"))))
   suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "eccc_ch"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "eccc_sar"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "iucn_amph"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "iucn_bird"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "iucn_rept"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "iucn_mamm"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_end"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_sar"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_spp"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "themes", "habitat"))))
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "species")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "species", "eccc_ch")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(
+      project_dir,
+      "tifs",
+      "themes",
+      "species",
+      "eccc_sar"
+    )))
+  )
+  suppressWarnings(
+    (dir.create(file.path(
+      project_dir,
+      "tifs",
+      "themes",
+      "species",
+      "iucn_amph"
+    )))
+  )
+  suppressWarnings(
+    (dir.create(file.path(
+      project_dir,
+      "tifs",
+      "themes",
+      "species",
+      "iucn_bird"
+    )))
+  )
+  suppressWarnings(
+    (dir.create(file.path(
+      project_dir,
+      "tifs",
+      "themes",
+      "species",
+      "iucn_rept"
+    )))
+  )
+  suppressWarnings(
+    (dir.create(file.path(
+      project_dir,
+      "tifs",
+      "themes",
+      "species",
+      "iucn_mamm"
+    )))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_end")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_sar")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "species", "nsc_spp")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "habitat")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "themes", "agriculture")))
+  )
   suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "carbon"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "climate"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "connectivity"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "eservices"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "pressures"))))
-  suppressWarnings((dir.create(file.path(project_dir, "tifs", "weights", "hotspots"))))
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "carbon")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "climate")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "connectivity")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "eservices")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "habitat")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "pressures")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "hotspots")))
+  )
+  suppressWarnings(
+    (dir.create(file.path(project_dir, "tifs", "weights", "tenure")))
+  )
   suppressWarnings((dir.create(file.path(project_dir, "tifs", "includes"))))
   suppressWarnings((dir.create(file.path(project_dir, "tifs", "excludes"))))
   suppressWarnings(dir.create(file.path(project_dir, "wtw")))
@@ -32,7 +104,7 @@ create_project_folder <- function(project_dir) {
   suppressWarnings(dir.create(file.path(project_dir, "dataprep", "national")))
   suppressWarnings(dir.create(file.path(project_dir, "dataprep", "regional")))
   suppressWarnings(dir.create(file.path(project_dir, "dataprep", "tmp")))
-  
+
   # Add csv and save to dataprep
   data.frame(
     conversion_ready_input = "full filepath to input data",
